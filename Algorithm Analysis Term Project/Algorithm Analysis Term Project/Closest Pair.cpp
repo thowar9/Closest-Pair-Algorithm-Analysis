ead21e817bcc2c55936a5f3412ea31a343d81ee2
thowar9 <thowar9@students.towson.edu>
2017-12-05 18:49:37 -0500

    data population update

--- Algorithm Analysis Term Project/Algorithm Analysis Term Project/Closest Pair.cpp
+++ Algorithm Analysis Term Project/Algorithm Analysis Term Project/Closest Pair.cpp
@@ -9,10 +9,13 @@ using namespace std;
 
 int main()
 {
-	int n = 1000; //initial input 
+	int n = 1000; //initial input, also input size counter
+	const int MAXINPUT = 100000; //max input size
+	const int PLANESIZE = 100; //plane size ex(100 by 100)
 	int trial = 1; //to keep track of trial #
 	int random = 0; //create the random variable
 	srand(time(NULL)); //seed the random variable
+	pair<int, int> data[MAXINPUT];
 
 	//Test file creation
 	ofstream myfile;
@@ -29,27 +32,33 @@ int main()
 		myfile << "Trial imput size: " << n << " "; 
 
 		//create input files here
-		random = ((rand() % 100) + 1); 
+		for (int i = 1; i <= n; i++)
+		{
+			data[i].first = ((rand() % PLANESIZE) + 1); 
+			data[i].second = ((rand() % PLANESIZE) + 1);
+		}
 
 		auto start = chrono::steady_clock::now(); 
-		
 		//algorithm logic goes here 
+
 
 
 
 
 		auto end = chrono::steady_clock::now(); 
 
-		myfile << "Elapsed time (s): " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " ";
-		myfile << "Value: " << random << endl;
+		myfile << "Elapsed time (s): " << chrono::duration_cast<chrono::seconds>(end - start).count() << " " << endl;
+		//myfile << "Value: " << random << endl;
 
 		n = n + 10; //incriment input size
 		trial++; //incriment trial
-	} while (n <= 100000); 
+	} while (n <= MAXINPUT);
 	//-------------------------------------------------------------------------------------
+
 	myfile.close();
 
-	std::cout << "test";
+	std::cout << "test complete " << endl;;
 	std::system("pause");
+
 	return 0;
 }
