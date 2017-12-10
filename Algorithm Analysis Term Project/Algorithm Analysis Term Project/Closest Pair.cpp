0c9b9264ca7c586f3cc0069f06dbcd9e2a1ac579
thowar9 <thowar9@students.towson.edu>
2017-12-10 17:30:40 -0500

    Divide and conquer algorithm done
    Finsihed logic for divide and conquer algorithm. No long has vector subscript out of range error, but now overflows stack.

--- Algorithm Analysis Term Project/Algorithm Analysis Term Project/Closest Pair.cpp
+++ Algorithm Analysis Term Project/Algorithm Analysis Term Project/Closest Pair.cpp
@@ -7,16 +7,17 @@
 #include <vector>
 #include <algorithm>
 
-//increased stack reserve size to 8MB via properties->configuration properties->linker->system
+//increased stack reserve size to 32MB via properties->configuration properties->linker->system
+//it still overflows :( issue terminating the recursion? 
 
 using namespace std; 
 
 double dist(pair<int, int>, pair<int, int>); 
-//double divideAndConquer(vector <pair<int, int> >, vector <pair<int, int>>, int); 
 double recursiveClosest(vector<pair<int, int>>, vector<pair<int, int>>); 
 double bruteForceUtility(vector<pair<int, int>>); 
 double findMin(double, double); 
 bool sortbysec(const pair<int, int> &, const pair<int, int> &);
+double areaClosest(vector<pair<int, int>>, double);
 
 const int MAXINPUT = 100000; //max input size
 const int PLANESIZE = 1000; //plane size ex(100 by 100)
@@ -85,7 +86,8 @@ int main()
 			trial++;
 		}
 		average = (toAverage[0] + toAverage[1] + toAverage[2]) / 3.0; //avg trials
-		outDivConq << "Run: " << run << " Average: " << average << endl;
+		outDivConq << "Run: " << run << " Average: " << average << endl
+			<< "trial 1: " << toAverage[0] << " trial 2: " << toAverage[1] << " trial 3: \n";
 		cout << "Run: " << run << endl; 
 
 		n = n + SKIPBY; //incriment input size
@@ -94,84 +96,10 @@ int main()
 	//-------------------------------------------------------------------------------------
 	outDivConq.close(); 
 
-	/*
-	//ENSURE TEST FILE IS CREATED ON COMPUTER RUNNING FINAL TRIALS
-	outBruteForce.open("C:\\Users\\Tucker\\Documents\\BruteForceOutput.txt", ios::trunc); //trunc to delete previous test file 
-	outBruteForce.close();
-	outBruteForce.open("C:\\Users\\Tucker\\Documents\\BruteForceOutput.txt", ios::app); //open append mode to add new writes to file
-	outBruteForce << "Writing brute force trial output to a file:\n"; //write
-	//-------------------------------------------------------------------------------------
-	do { //brute force runs below
-		n = 1000; 
-		trial = 1; 
-		while (trial <= 3) 
-		{
-			//create input files here
-			for (int i = 0; i <= n-1; i++)
-			{
-				BFdata[i].first = ((rand() % PLANESIZE) + 1);
-				BFdata[i].second = ((rand() % PLANESIZE) + 1);
-			}
-
-			auto start = chrono::steady_clock::now();
-
-			//brute force algorithm logic goes here 
-			for (int i = 0; i < n; i++)
-			{
-				for (int j = (i + 1); j <= n; j++)
-				{
-					if (dist(BFdata[i], BFdata[j]) < minimumDistance)
-					{
-						minimumDistance = dist(BFdata[i], BFdata[j]);
-					}
-				}
-			}
-			auto end = chrono::steady_clock::now();
-
-			toAverage[trial-1] = chrono::duration_cast<chrono::milliseconds>(end - start).count(); 
-
-			outBruteForce << "Trial " << trial << ": " << toAverage[trial-1] << " "; 
-			trial++;
-		}
-
-		average = (toAverage[0] + toAverage[1] + toAverage[2])/3; 
-		outBruteForce << "Run: " << run << "Average: " << average << endl; 
-
-		n = n + SKIPBY; //incriment input size
-		run++; //incriment run number 
-
-	} while (n < MAXINPUT);
-	//-------------------------------------------------------------------------------------
-
-	outBruteForce.close();
-	*/
-
 	std::cout << "test complete " << endl; //ADD CORRECT STATEMENT, MOVE TO CORRECT SPOT 
 	std::system("pause");
 
 	return 0;
-}
-
-double dist(pair<int, int> a, pair<int, int> b)
-{
-	int distancex = (a.first - b.first)*(a.first - b.first); 
-	int distancey = (a.second - b.second)*(a.second - b.second); 
-	double distance = sqrt(abs(distancex - distancey)); 
-	return distance; 
-}
-
-
-double findMin(double first, double second)
-{
-	if (first < second) return first;
-	else return second; 
-}
-//to find the distance between closest points in given area
-
-// utility function to sort the vector elements by second element of pairs
-bool sortbysec(const pair<int, int> &a, const pair<int, int> &b)
-{
-	return (a.second < b.second);
 }
 
 double recursiveClosest(vector <pair<int, int>> dataX, vector <pair<int, int>> dataY)
@@ -207,19 +135,41 @@ double recursiveClosest(vector <pair<int, int>> dataX, vector <pair<int, int>> d
 		else
 			dataYright.push_back(dataY.at(i)); // Y sorted points on left side of line
 	}
+	dataX.clear();
+	dataY.clear();
 
 	distanceLeft = recursiveClosest(dataXleft, dataYleft); //recursive call for left side
 	cout << "made it through left side\n"; //TEST STATEMENT
+	dataXleft.clear();
+	dataYleft.clear();
 
-	distanceRight = recursiveClosest(dataXright, dataYleft); //recursive call for right side 
+	distanceRight = recursiveClosest(dataXright, dataYright); //recursive call for right side 
 	cout << "made it through right side\n"; //TEST STATEMENT
+	dataXright.clear();
+	dataYright.clear(); 
 
-	minimumDistance = findMin(distanceLeft, distanceRight); 
+	minimumDistance = findMin(distanceLeft, distanceRight); //decide which has shorter distance
+	distanceMiddle = areaClosest(dataY, minimumDistance); //find shortest distance in middle strip "area"
 
-	//currently ignoring third case where closest point is split by middle line 
-
+	minimumDistance = findMin(minimumDistance, distanceMiddle); 
 
 	return minimumDistance; 
+}
+
+double areaClosest(vector<pair<int, int>> dataY, double d)
+{
+	double min = d; 
+	for (int i = 0; i < dataY.size(); ++i)
+	{
+		for (int j = i + 1; j < dataY.size() && (dataY.at(j).second - dataY.at(i).second) < min; ++j)
+		{
+			if (dist(dataY.at(i), dataY.at(j)) < min)
+			{
+				min = dist(dataY.at(i), dataY.at(j)); 
+			}
+		}
+	}
+	return min; 
 }
 
 double bruteForceUtility (vector <pair<int, int>> data) //utility for recursiveClosest
@@ -229,7 +179,7 @@ double bruteForceUtility (vector <pair<int, int>> data) //utility for recursiveC
 	cout << "start brute force utility--"; //TEST STATEMENT
 	for (int i = 0; i < data.size() - 1; i++)
 	{
-		for (int j = (i + 1); j < data.size() - 1; j++)
+		for (int j = (i + 1); j < data.size(); j++)
 		{
 			currentDistance = dist(data.at(i), data.at(j));
 			if (currentDistance < minimumDistance)
@@ -240,4 +190,24 @@ double bruteForceUtility (vector <pair<int, int>> data) //utility for recursiveC
 	}
 	cout << "--end brute force utility\n"; //TEST STATEMENT
 	return minimumDistance; 
+}
+
+double dist(pair<int, int> a, pair<int, int> b)
+{
+	int distancex = (a.first - b.first)*(a.first - b.first);
+	int distancey = (a.second - b.second)*(a.second - b.second);
+	double distance = sqrt(abs(distancex + distancey));
+	return distance;
+}
+
+double findMin(double first, double second)
+{
+	if (first < second) return first;
+	else return second;
+}
+
+// utility function to sort the vector elements by second element of pairs
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b)
+{
+	return (a.second < b.second);
 }
